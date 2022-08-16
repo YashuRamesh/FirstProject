8b7c831263ffdfedb5ed5fb1ce128327e27a3727
GitFirstProject <itsmeyashaswini@gmail.com>
2022-08-16 17:10:17 +0530

    My first commit in Git here

---
+++ src/test/java/first/TestCase1.java
@@ -0,0 +1,19 @@
+package first;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	@Test
+	public void display() {
+		System.out.println("welcome");
+	}
+	
+	@Test
+	public void display2() {
+		System.out.println("hello");
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/first/TestCase2.java
@@ -0,0 +1,10 @@
+package first;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+  @Test
+  public void f() {
+	  System.out.println("method2");
+  }
+}
